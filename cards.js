d0651e4835140692192534aaaba1407cecbd2713
snehal122 <88581301+snehal122@users.noreply.github.com>
2021-10-23 13:45:50 +0530

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Users = ({ loading, users }) => {
+  const mainstyle = {
+    display: "flex"
+  };
+  return loading ? (
+    <div id="main" align="center" color="transparent">
+      <img
+        src="https://i.gifer.com/1fpC.gif"
+        alt="Loaading.."
+        className="loader"
+      />
+    </div>
+  ) : (
+    <div id="main" style={mainstyle}>
+      {users.map((user) => (
+        <div className="profile_cont" key={user.id}>
+          {console.log(user)}
+
+          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+          <h1 className="name">
+            {user.first_name} {user.last_name}
+          </h1>
+          <p className="email">{user.email}</p>
+          <p className="userid">User ID : {user.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
